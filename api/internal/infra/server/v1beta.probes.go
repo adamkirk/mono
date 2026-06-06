6419db736465e97b8c69eab909f07c801b7784b4
Adam Kirk <adam_kirk@live.co.uk>
2026-06-06 20:38:07 +0100

    chore(api): add ready probe
    Add a probe to check that the app is ready to serve traffic.

--- api/internal/infra/server/v1beta.probes.go
+++ api/internal/infra/server/v1beta.probes.go
@@ -27,6 +27,21 @@ func (c *V1BetaProbesController) RegisterRoutes(v ApiVersion, api huma.API) {
 			OptDisableDefaultAuthentication: true,
 		},
 	}, ErrorHandler(c.Startup, http.MethodGet))
+
+	huma.Register(api, huma.Operation{
+		OperationID:   fmt.Sprintf("%s.probes.ready", string(v)),
+		Method:        http.MethodGet,
+		Path:          "/_/probes/ready",
+		Summary:       "Check if the app is ready to serve connections",
+		DefaultStatus: http.StatusNoContent,
+		Tags: []string{
+			"Probes",
+		},
+		Metadata: map[string]any{
+			OptDisableAllDefaultResponses:   true,
+			OptDisableDefaultAuthentication: true,
+		},
+	}, ErrorHandler(c.Ready, http.MethodGet))
 }
 
 func NewProbesController() *V1BetaProbesController {
@@ -34,6 +49,12 @@ func NewProbesController() *V1BetaProbesController {
 }
 
 func (c *V1BetaProbesController) Startup(ctx context.Context, req *V1BetaProbesStartupRequest) (*NoContent, error) {
+	return &NoContent{
+		Status: http.StatusNoContent,
+	}, nil
+}
+
+func (c *V1BetaProbesController) Ready(ctx context.Context, req *V1BetaProbesStartupRequest) (*NoContent, error) {
 	return &NoContent{
 		Status: http.StatusNoContent,
 	}, nil
